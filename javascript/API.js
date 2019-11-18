62764ee03ca84fb4aec5a3fe65770aefad6f81dd
fmmMelo <felipe.moura@estudante.ifms.edu>
2019-11-17 23:18:29 -0300

    alteração dados dos locais

--- javascript/API.js
+++ javascript/API.js
@@ -5,18 +5,19 @@ function mapaMultiplosMarcadores(){
       { 
         nome: 'IFMS',
         descricao: 'Instituto Federal de Mato Grosso do Sul - Campus Dourados',
+        endereco: 'Endereço: R. Filinto Müller, 1790 - Canaã I, Dourados - MS, 79833-520',
+        pais: 'Pais: Brasil', 
+        gm: '<a style="text-decoration: none;" href="https://www.google.com/maps/place/Instituto+Federal+de+Mato+Grosso+do+Sul+(IFMS)/@-22.2089873,-54.7670866,17z/data=!3m1!4b1!4m5!3m4!1s0x9489a8cb0944a387:0xb5931d768f1f95fe!8m2!3d-22.2089923!4d-54.7648979">Visualize no Google Maps</a>',
         latitudeLongitude: { lat: -22.208694,  lng: -54.764941 }
       },
       { 
         nome: 'Lar Santa Rita',
-        descricao: 'Lar Santa Rita de Dourados',
+        descricao: 'Lar Santa Rita - Dourados',
+        endereco: 'Endereço: R. Toshinobu Katayama, 1033 - Vila Planalto, Dourados - MS, 79826-110',
+        pais: 'Pais: Brasil', 
+        gm: '<a style="text-decoration: none;" href="https://www.google.com/maps/place/Lar+Santa+Rita/@-22.2224646,-54.8072764,17z/data=!3m1!4b1!4m5!3m4!1s0x9489a835189397bf:0xb0d8c351de976f2d!8m2!3d-22.2224696!4d-54.8050877">Visualize no Google Maps</a>',
         latitudeLongitude: { lat: -22.2224646,  lng: -54.8050877 }
       },
-      { 
-        nome: 'Usina Velha',
-        descricao: 'Usina Velha: ponto turístico de dourados',
-        latitudeLongitude: { lat: -22.2104014, lng: -54.819986 }
-      }
     ];  
     
     mapaMultiplosMarcadores = new google.maps.Map(divMapaMarcadores, {
@@ -35,12 +36,15 @@ function mapaMultiplosMarcadores(){
       }); 
       
       /* Conteúdo HTML que representa a janela de informações que aparece quando o marcador é selecionado. Essa estrutura HTML é padrão da API de mapas do Google */
-      let conteudoJanelaInformacoes = '<div id="content">'+
+      let conteudoJanelaInformacoes = '<div id="content" style="text-align: justify;">'+
               '<div id="siteNotice">'+
               '</div>'+
               '<h1 id="firstHeading" class="firstHeading">'+ local.nome + '</h1>'+
               '<div id="bodyContent">'+
-                '<p>' + local.descricao + '</p>'+
+              '<p>' + local.descricao + '</p>'+
+              '<p>' + local.endereco + '</p>'+
+              '<p>' + local.pais + '</p>'+
+              '<p>'+local.gm+'</p>'+
               '</div>'+
           '</div>';
   
