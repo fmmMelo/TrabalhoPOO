f299116fb440a777bf62702fc36dd3f8e2bf271d
fmmMelo <felipe.moura@estudante.ifms.edu>
2019-11-19 23:30:22 -0300

    adicionando pontos no maps

--- javascript/API.js
+++ javascript/API.js
@@ -18,6 +18,14 @@ function mapaMultiplosMarcadores(){
         gm: '<a style="text-decoration: none;" href="https://www.google.com/maps/place/Lar+Santa+Rita/@-22.2224646,-54.8072764,17z/data=!3m1!4b1!4m5!3m4!1s0x9489a835189397bf:0xb0d8c351de976f2d!8m2!3d-22.2224696!4d-54.8050877">Visualize no Google Maps</a>',
         latitudeLongitude: { lat: -22.2224646,  lng: -54.8050877 }
       },
+      { 
+        nome: 'Casa Criança Feliz',
+        descricao: 'Casa Criança Feliz - Dourados',
+        endereco: 'R. Ramão Osório, 535 - Vila Sao Braz, Dourados - MS, 79843-250',
+        pais: 'Pais: Brasil', 
+        gm: '<a style="text-decoration: none;" href="https://www.google.com/search?q=casa%20da%20crian%C3%A7a%20Dourados&oq=casa&aqs=chrome.2.69i57j0j69i59j0j69i60l4.1991j0j7&sourceid=chrome&ie=UTF-8&safe=active&sxsrf=ACYBGNQfJntpLoQiVsLw3QZ-VPKH2yTHCg:1574216857342&npsic=0&rflfq=1&rlha=0&rllag=-22233992,-54772312,3526&tbm=lcl&rldimm=6103437971992267034&ved=2ahUKEwjdgtPk3vflAhWaErkGHW48CucQvS4wAHoECAsQIA&rldoc=1&tbs=lrf:!1m4!1u3!2m2!3m1!1e1!1m4!1u2!2m2!2m1!1e1!2m1!1e2!2m1!1e3!3sIAE,lf:1,lf_ui:2&rlst=f#rlfi=hd:;si:;mv:[[-22.2105993,-54.7350154],[-22.2452397,-54.8198855]]">Visualize no Google Maps</a>',
+        latitudeLongitude: { lat: -22.2452397,  lng: -54.8198855 }
+      },
     ];  
     
     mapaMultiplosMarcadores = new google.maps.Map(divMapaMarcadores, {
